0f6198c31eb1875cccc21acc72fa2451960e715a
Loctarogar <igrsl1990@gmail.com>
2020-02-23 21:04:59 +0100

    can create boxes and delete them

--- bouncing_square.py
+++ bouncing_square.py
@@ -11,10 +11,10 @@ class Stick():
         self.x = 200
         self.y = 120
         self.width = 200
-        self.hight = 15
+        self.height = 15
         
     def draw(self, screen):
-        pygame.draw.rect(screen, RED, (self.x, self.y, self.width, self.hight))
+        pygame.draw.rect(screen, RED, (self.x, self.y, self.width, self.height))
         
 stick = Stick()
 
@@ -23,25 +23,40 @@ class Square():
         self.x = 390
         self.y = 480
         self.width = 20
-        self.hight = 20
+        self.height = 20
         self.y_vel = 2
         self.x_vel = 2
         
     def draw(self, screen):
-        pygame.draw.rect(screen, RED, (self.x, self.y, self.width, self.hight))
+        pygame.draw.rect(screen, RED, (self.x, self.y, self.width, self.height))
         self.y += self.y_vel
         self.x += self.x_vel
         # bouncing from display borders
-        if self.y + self.hight >= size[1] or self.y <= 1:
+        if self.y + self.height >= size[1] or self.y <= 1:
             self.y_vel = self.y_vel * -1
         if self.x + self.width >= size[0] or self.x <= 1:
             self.x_vel = self.x_vel * -1
         #bouncing from stick
-            
-        if self.y <= stick.y + stick.hight:
-            if self.x >= stick.x and self.x + self.width <= stick.x + stick.width and self.y + self.hight > stick.y:
+        if self.y <= stick.y + stick.height:
+            if self.x >= stick.x and self.x + self.width <= stick.x + stick.width and self.y + self.height > stick.y:
                 self.y_vel *= -1
-
+        
+    def hit_box(self, box):
+        if self.y + self.height > box.y:
+            if self.x + self.width > box.x and self.x + self.width < box.x + box.width:
+                return True
+            return False
+                
+class Box():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 30
+        self.height = 30
+        
+    def draw(self, screen):
+        pygame.draw.rect(screen, GREEN,(self.x, self.y, self.width, self.height))
+        
 pygame.init()
  
 # Set the width and height of the screen [width, height]
@@ -68,7 +83,16 @@ def redrawGameWindow():
     screen.blit(background, (0, 0))
     square.draw(screen)
     stick.draw(screen)
- 
+    for box in boxes:
+        box.draw(screen)
+
+step = 50
+boxes = [];
+for box in range(0, 20):
+    boxes.append(Box(step,570))
+    step += 35
+for box in boxes:
+    print (box.x)    
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -84,6 +108,9 @@ while not done:
         stick.x += 4
  
     # --- Game logic should go here
+    for box in boxes:
+        if square.hit_box(box):
+            boxes.pop(boxes.index(box))
  
     # --- Screen-clearing code goes here
  
